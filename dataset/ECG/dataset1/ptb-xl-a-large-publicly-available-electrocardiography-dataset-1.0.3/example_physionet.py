aa5dedf1dde5e6a1407f6e238f8571d250bcb9d8
Daniel Peter Mohr <38252161+Danimo93@users.noreply.github.com>
2025-10-06 17:43:19 +0200

    Add new data

--- dataset/ECG/dataset1/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.3/example_physionet.py
+++ dataset/ECG/dataset1/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.3/example_physionet.py
@@ -3,27 +3,26 @@ import numpy as np
 import wfdb
 import ast
 
-def load_raw_data(df, sampling_rate):
+def load_raw_data(df, sampling_rate, path):
     if sampling_rate == 100:
-        data = [wfdb.rdsamp(f) for f in df.filename_lr]
-        print(sampling_rate)
+        data = [wfdb.rdsamp(path+f) for f in df.filename_lr]
     else:
-        data = [wfdb.rdsamp(f) for f in df.filename_hr]
+        data = [wfdb.rdsamp(path+f) for f in df.filename_hr]
     data = np.array([signal for signal, meta in data])
     return data
 
-
+path = 'path/to/ptbxl/'
 sampling_rate=100
 
 # load and convert annotation data
-Y = pd.read_csv('ptbxl_database.csv', index_col='ecg_id')
+Y = pd.read_csv(path+'ptbxl_database.csv', index_col='ecg_id')
 Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
 
 # Load raw signal data
-X = load_raw_data(Y, sampling_rate)
+X = load_raw_data(Y, sampling_rate, path)
 
 # Load scp_statements.csv for diagnostic aggregation
-agg_df = pd.read_csv('scp_statements.csv', index_col=0)
+agg_df = pd.read_csv(path+'scp_statements.csv', index_col=0)
 agg_df = agg_df[agg_df.diagnostic == 1]
 
 def aggregate_diagnostic(y_dic):
@@ -44,8 +43,3 @@ y_train = Y[(Y.strat_fold != test_fold)].diagnostic_superclass
 # Test
 X_test = X[np.where(Y.strat_fold == test_fold)]
 y_test = Y[Y.strat_fold == test_fold].diagnostic_superclass
-
-print(X_train.shape, len(y_train))
-print(X_test.shape, len(y_test))
-print(y_train.value_counts())
-print(y_test.value_counts())
